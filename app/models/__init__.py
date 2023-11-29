e434a0f4923bfe676fdf40dd3c933f32c450f92d
Andrei Vorobev <andrey.vorob1995@gmail.com>
2023-11-29 18:50:27 -0500

    models are fully complete

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,5 +4,5 @@ from .reviews import Review
 from .orders import Order
 from .order_products import OrderProduct
 from .items import Item
-from favorite_products import FavoriteProduct
+from .favorite_products import FavoriteProduct
 from .db import environment, SCHEMA
