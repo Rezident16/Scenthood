8018dc10951bea62fe46e31a3a3b69696d292707
Andrei Vorobev <andrey.vorob1995@gmail.com>
2023-12-08 16:21:25 -0500

    loader on items

--- react-app/src/components/Items/items.js
+++ react-app/src/components/Items/items.js
@@ -3,9 +3,20 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchItems } from "../../store/items";
 import ItemTile from "./itemTile";
 import "./items.css";
+import LoaderComp from "./loader";
 
 function ItemsComponent() {
   const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(true);
+  const [seconds, setSeconds] = useState(1);
+
+  useEffect(() => {
+    seconds > 0 && setTimeout(() => setSeconds(seconds - 1), 1000);
+  }, [seconds]);
+
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 1200);
 
   useEffect(() => {
     (async () => {
@@ -18,13 +29,26 @@ function ItemsComponent() {
 
   if (!items) return null;
   return (
-    <div className="all_items">
-      {items &&
-        items
-          .sort((a, b) => b.reviews.length - a.reviews.length)
-          .map((item) => (
-            <ItemTile className="individual_item" key={item.id} item={item} />
-          ))}
+    <div>
+      {isLoading ? (
+        <div  className="countdown">
+            <LoaderComp />
+
+           </div> 
+      ) : (
+        <div className="all_items">
+          {items &&
+            items
+              .sort((a, b) => b.reviews.length - a.reviews.length)
+              .map((item) => (
+                <ItemTile
+                  className="individual_item"
+                  key={item.id}
+                  item={item}
+                />
+              ))}
+        </div>
+      )}
     </div>
   );
 }
