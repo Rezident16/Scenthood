a9fb28f099977c07fe008239ac00360b9b3111cb
Andrei Vorobev <andrey.vorob1995@gmail.com>
2024-04-10 14:06:33 -0400

    cosmetic changes on hamburger dropdown

--- react-app/src/components/Navigation/Hamburger.js
+++ react-app/src/components/Navigation/Hamburger.js
@@ -5,22 +5,20 @@ import { logout } from "../../store/session";
 import ItemForm from "../Items/CreateUpdateItemForm";
 import { useHistory } from "react-router-dom";
 
-function UserHamburger ({userClass, user}) {
-
-    const dispatch = useDispatch();
+function UserHamburger({ userClass, user }) {
+  const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
   const ulRef = useRef();
   const history = useHistory();
   let isCompleted = true;
   if (user) {
-    isCompleted = user.address != 'None' ? true : false;
+    isCompleted = user.address != "None" ? true : false;
   }
 
   const openMenu = async () => {
     if (showMenu) return;
     setShowMenu(true);
   };
-
 
   useEffect(() => {
     if (!showMenu) return;
@@ -51,60 +49,58 @@ function UserHamburger ({userClass, user}) {
   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
   const closeMenu = () => setShowMenu(false);
 
-
-    return (
-        <div className={userClass}>
-        <button id="hamburger" onClick={openMenu}>
-          <i
-            class="fa fa-bars"
-            aria-hidden="true"
-            style={{ fontSize: "30px", color: "black" }}
-          />
-        </button>
-        <ul className={ulClassName} ref={ulRef}>
-          <div className="profile_text">
-            <li>{user?.username}</li>
-            {/* <li>{user?.email}</li> */}
-            <li>
-              {user?.first_name} {user?.last_name}
-            </li>
-            <li className="seperator"></li>
-            {isCompleted? (
-              <div className="hamburger_buttons">
-            <li className="profile_button" onClick={goToProfile}>
-              My Profile
-            </li>
-            <li onClick={closeMenu}>
-              <OpenModalDiv
-                onItemClick={closeMenu}
-                className={"profile_button"}
-                buttonText="Sell Your Item"
-                modalComponent={<ItemForm formType="create" />}
-              />
-            </li>
-              </div>
-
-            ) : (
-            <li onClick={closeMenu}>
-              <button className="profile_button"
-              onClick={() => {
-                history.push(`/complete`)
-              }}
-              >
+  return (
+    <div className={userClass}>
+      <button id="hamburger" onClick={openMenu}>
+        <i
+          class="fa fa-bars"
+          aria-hidden="true"
+          style={{ fontSize: "30px", color: "black" }}
+        />
+      </button>
+      <ul className={ulClassName} ref={ulRef}>
+        <div className="profile_text">
+          <li>{user?.username}</li>
+          {/* <li>{user?.email}</li> */}
+          <li>
+            {user?.first_name} {user?.last_name}
+          </li>
+          <li className="seperator"></li>
+          {isCompleted ? (
+            <div className="hamburger_buttons">
+              <li className="profile_button" onClick={goToProfile}>
+                My Profile
+              </li>
+              <li onClick={closeMenu}>
+                <OpenModalDiv
+                  onItemClick={closeMenu}
+                  className={"profile_button"}
+                  buttonText="Sell Your Item"
+                  modalComponent={<ItemForm formType="create" />}
+                />
+              </li>
+            </div>
+          ) : (
+            <li onClick={closeMenu}
+            className="hamburger_buttons"
+            style={{ display: "flex", flexDirection: "column", marginTop: "10px"}}
+            >
+              <li className="profile_button" onClick={() => {
+                  history.push(`/complete`);
+                }}>
                 Complete Profile
-              </button>
+              </li>
             </li>
-
-            )}
-            <li className="logout_button_li">
-              <div onClick={handleLogout} className="logout_button">
-                Log Out
-              </div>
-            </li>
-          </div>
-        </ul>
-      </div>
-    )
+          )}
+          <li className="logout_button_li">
+            <div onClick={handleLogout} className="logout_button">
+              Log Out
+            </div>
+          </li>
+        </div>
+      </ul>
+    </div>
+  );
 }
 
 export default UserHamburger;
