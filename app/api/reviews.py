d580f6b7606f4d3ddf586d4d30b452cd403914e2
Andrei Vorobev <andrey.vorob1995@gmail.com>
2024-05-02 11:06:28 -0400

    clean up reviews api

--- app/api/reviews.py
+++ app/api/reviews.py
@@ -1,50 +1,57 @@
-from flask import Blueprint, jsonify, request, abort, request
+from flask import Blueprint, jsonify, request, abort
 from app.models import *
 from flask_login import current_user, login_required
-from datetime import time, datetime, date
+from datetime import datetime
 from app.forms import *
-from .aws_helpers import *
 
 review_routes = Blueprint('reviews', __name__)
 
-# Delete Review reviews/:id
-@review_routes.route('<int:reviewId>', methods=["DELETE"])
-def delete_review(reviewId):
-    review = Review.query.get(reviewId)
+def get_review(reviewId):
+    return Review.query.get(reviewId)
 
-    if not review:
-        return {}
-    
-    if review.order.user_id != current_user.id:
-        return abort(403, description='Unauthorized')
-    
+def validate_form(form):
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        return form.data
+    else:
+        return None
+
+def delete_review_from_db(review):
     db.session.delete(review)
     db.session.commit()
 
-    return {"status": "success"}, 200
-# Update Review reviews/:id
-@review_routes.route('<int:reviewId>', methods=["POST"])
-def update_review(reviewId):
-    review = Review.query.get(reviewId)
-
+@review_routes.route('<int:reviewId>', methods=["DELETE"])
+def delete_review(reviewId):
+    review = get_review(reviewId)
     if not review:
         return {}
-    
+
+    if review.order.user_id != current_user.id:
+        return abort(403, description='Unauthorized')
+
+    delete_review_from_db(review)
+    return {"status": "success"}, 200
+
+def update_review_data(review, data):
+    review.note = data['note']
+    review.stars = data['stars']
+    review.updated_at = datetime.now()
+    db.session.commit()
+    return review
+
+@review_routes.route('<int:reviewId>', methods=["POST"])
+def update_review(reviewId):
+    review = get_review(reviewId)
+    if not review:
+        return {}
+
     form = ReviewForm()
-    form['csrf_token'].data = request.cookies['csrf_token']
-
-    if form.validate_on_submit():
-        data = form.data
-
-        if review.order.user_id != current_user.id:
-            return abort(403, description='Unauthorized')
-    
-        review.note = data['note']
-        review.stars = data['stars']
-        review.updated_at = datetime.now()
-        
-        db.session.commit()
-        return review.to_dict_self(), 200
-    
-    if form.errors:
+    data = validate_form(form)
+    if data is None:
         return form.errors
+
+    if review.order.user_id != current_user.id:
+        return abort(403, description='Unauthorized')
+
+    updated_review = update_review_data(review, data)
+    return updated_review.to_dict_self(), 200
