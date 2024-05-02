9449f739061748cd06c6bee3c4b59e1625c09f47
Andrei Vorobev <andrey.vorob1995@gmail.com>
2024-05-02 11:07:35 -0400

    clean up user_routes

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,90 +1,73 @@
 from flask import Blueprint, jsonify, session, request
-from flask_login import login_required,current_user
+from flask_login import login_required, current_user
 from app.models import User, Order
 from app.forms import SignUpForm
 from .aws_helpers import *
-from app.models import *
-from app.forms import *
 
 user_routes = Blueprint('users', __name__)
 
+def get_user_by_id(userId):
+    return User.query.get(userId)
+
+def validate_form(form):
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        return form.data
+    else:
+        return None
+
+def update_user_data(user, data, image):
+    image.filename = get_unique_filename(image.filename)
+    upload = upload_file_to_s3(image)
+
+    remove_file_from_s3(user.profile_img)
+    user.profile_img = upload["url"]
+    user.username = data['username']
+    user.first_name = data['first_name']
+    user.last_name = data['last_name']
+    user.address = data['address']
+    user.city = data['city']
+    user.state = data['state']
+    user.description = data['description']
+
+    db.session.commit()
+    return user
 
 @user_routes.route('/')
 @login_required
 def users():
-    """
-    Query for all users and returns them in a list of user dictionaries
-    """
     users = User.query.all()
     return {'users': [user.to_dict_self() for user in users]}
 
-
-
-
 @user_routes.route('/current')
 @login_required
-def current(id):
-    """
-    Query for a user by id and returns that user in a dictionary
-    """
-    user = User.query.get(current_user.id)
+def current():
+    user = get_user_by_id(current_user.id)
     return user.to_dict_self()
 
 @user_routes.route('/<int:userId>')
 def user(userId):
-    """
-    Query for a user by id and returns that user in a dictionary
-    """
-    try:
-        user = User.query.get(userId)
-    except AttributeError:
-        return None
-    return user.to_dict_self()
+    user = get_user_by_id(userId)
+    return user.to_dict_self() if user else None
 
 @user_routes.route('/<int:userId>/edit', methods=["POST"])
 @login_required
 def edit_user(userId):
-    """
-    Edits a user
-    """
-    form = OauthSignUpForm()
-    form['csrf_token'].data = request.cookies['csrf_token']
-    user = User.query.get(userId)
-
-    if form.validate_on_submit():
-        data = form.data
-        image = data['profile_img']
-        image.filename = get_unique_filename(image.filename)
-        upload = upload_file_to_s3(image)
-
-        remove_file_from_s3(user.profile_img)
-        user.profile_img = upload["url"]
-        user.username = data['username']
-        user.first_name = data['first_name']
-        user.last_name = data['last_name']
-        user.address = data['address']
-        user.city = data['city']
-        user.state = data['state']
-        user.description = data['description']
-
-        db.session.commit()
-        return user.to_dict_self()
-    
-    if form.errors:
+    form = SignUpForm()
+    data = validate_form(form)
+    if data is None:
         return form.errors
+
+    user = get_user_by_id(userId)
+    updated_user = update_user_data(user, data, data['profile_img'])
+    return updated_user.to_dict_self()
 
 @user_routes.route('/<int:userId>/orders/<int:orderId>')
 def order(orderId):
-    try:
-        order = Order.query.get(orderId)
-    except AttributeError:
-        return None
-    return order.to_dict_self()
+    order = Order.query.get(orderId)
+    return order.to_dict_self() if order else None
 
 @user_routes.route('/<int:userId>/orders')
 def orders(userId):
-    try:
-        orders = Order.query.filter(Order.user_id == userId).all()
-    except AttributeError:
-        return None
+    orders = Order.query.filter(Order.user_id == userId).all()
     return [order.to_dict_self() for order in orders]
